8da8c8a850888e74bad49cbb3a9686399e111b1f
Cpt.Captain <nilskoch@posteo.de>
2023-01-02 15:51:25 +0100

    update coco dataset path

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,6 +1,7 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'datasets/coco/'
+# data_root = 'datasets/coco/'
+data_root = '/home/nils/VAN-Detection/datasets/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
