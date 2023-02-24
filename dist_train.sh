7b5de439225efad5a88afd1485b54d8ba006f845
Cpt.Captain <nilskoch@posteo.de>
2023-02-24 10:40:38 +0100

    clean-up

--- dist_train.sh
+++ dist_train.sh
@@ -6,22 +6,13 @@ PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-# torchrun train.py $CONFIG
 
-torchrun \
+python -m torch.distributed.launch \
     --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
-    --rdzv_endpoint=$MASTER_ADDR:$PORT \
+    --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
+    --master_port=$PORT \
     $(dirname "$0")/train.py \
     $CONFIG \
     --launcher pytorch ${@:3}
-# python -m torch.distributed.launch \
-#     --nnodes=$NNODES \
-#     --node_rank=$NODE_RANK \
-#     --master_addr=$MASTER_ADDR \
-#     --nproc_per_node=$GPUS \
-#     --master_port=$PORT \
-#     $(dirname "$0")/train.py \
-#     $CONFIG \
-#     --launcher pytorch ${@:3}
