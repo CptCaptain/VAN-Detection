df4d55c1100995fbae82a6f452b28173d54bd41f
Cpt.Captain <nilskoch@posteo.de>
2023-02-24 10:45:27 +0100

    fix workflow, change dist backend to nccl

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -20,10 +20,11 @@ log_config = dict(
 # yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
 
-dist_params = dict(backend='gloo')
+# dist_params = dict(backend='gloo')
+dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
 resume_from = None
-workflow = [('train', 1), ('val', 1)]
+workflow = [('train', 1)]
 cudnn_benchmark = True
 # checkpoint_config = dict(interval=1, max_keep_ckpts=3)
