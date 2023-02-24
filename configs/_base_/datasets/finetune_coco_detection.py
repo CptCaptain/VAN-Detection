03e90c71d86d41fb45206aaca5678556500c05ab
Cpt.Captain <nilskoch@posteo.de>
2023-02-24 10:43:55 +0100

    fix val and test config, clean-up, general improvements

--- configs/_base_/datasets/finetune_coco_detection.py
+++ configs/_base_/datasets/finetune_coco_detection.py
@@ -1,9 +1,5 @@
-
 custom_imports = dict(imports=['finetunecopypaste'], allow_failed_imports=False)
 
-# _base_ = [
-    # '../datasets/coco_detection.py',
-# ]
 CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
@@ -17,11 +13,12 @@ CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
                'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
-               'vase', 'scissors', 'teddy bear', 'hair drier', 'Audi_RS_6_Avant')
+               # 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
+               'vase', 'scissors', 'teddy bear', 'Audi_A7', 'Audi_RS_6_Avant')
 
 dataset_type = 'CocoDataset'
-# data_root = 'datasets/coco/'
 data_root = '/home/nils/VAN-Detection/datasets/coco/'
+
 image_size = (1024, 1024)
 file_client_args = dict(backend='disk')
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
@@ -32,36 +29,29 @@ albu_train_transforms = [
         transforms=[
             dict(
                 type='Resize',
-                width=image_size[0],
-                height=image_size[1],
+                width=image_size[1],
+                height=image_size[0],
             ),
             dict(
                 type='RandomResizedCrop',
-                width=image_size[0],
-                height=image_size[1],
+                width=image_size[1],
+                height=image_size[0],
             ),
         ],
         p=1.0
     ),
     dict(
         type='ShiftScaleRotate',
-        # shift_limit=0.0625,
-        # scale_limit=0.0,
-        # rotate_limit=0,
+        scale_limit=[-0.9,0.0],
+        rotate_limit=0,
         border_mode=0,
         rotate_method='ellipse',
         interpolation=1,
         p=0.5),
-    # dict(
-        # type='Rotate',
-        # rotate_method='ellipse',
-        # border_mode=0,
-        # p=0.5,
-    # ),
     dict(
         type='RandomBrightnessContrast',
-        # brightness_limit=[0.1, 0.3],
-        # contrast_limit=[0.1, 0.3],
+        brightness_limit=[0.1, 0.3],
+        contrast_limit=[0.1, 0.3],
         p=0.5),
     dict(
         type='OneOf',
@@ -100,73 +90,14 @@ albu_train_transforms = [
         type='Flip',
         ),
     dict(
-        type='PadIfNeeded',
-        min_width=image_size[0],
-        min_height=image_size[1],
-        border_mode=0,
-        p=1.0,
-        ),
-]
-
-albu_val_transforms = [
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(
-                type='Resize',
-                width=image_size[0],
-                height=image_size[1],
-            ),
-            dict(
-                type='RandomResizedCrop',
-                width=image_size[0],
-                height=image_size[1],
-            ),
-        ],
-        p=1.0
-    ),
-    dict(
-        type='ShiftScaleRotate',
-        border_mode=0,
-        rotate_method='ellipse',
-        interpolation=1,
-        p=0.5),
-    dict(
-        type='RandomBrightnessContrast',
-        p=0.5),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(
-                type='RGBShift',
-                p=1.0),
-            dict(
-                type='HueSaturationValue',
-                hue_shift_limit=128,
-                sat_shift_limit=128,
-                val_shift_limit=128,
-                p=1.0),
-        ],
-        p=0.9),
-    dict(type='ChannelShuffle', p=0.3),
-    dict(type='GaussNoise', p=0.8),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(type='AdvancedBlur', p=1.0),
-            dict(type='MedianBlur', blur_limit=3, p=1.0),
-        ],
-        p=0.4),
-    dict(
-        type='ColorJitter',
-        ),
-    dict(
-        type='Flip',
+        type='Resize',
+        width=image_size[1],
+        height=image_size[0],
         ),
     dict(
         type='PadIfNeeded',
-        min_width=image_size[0],
-        min_height=image_size[1],
+        min_width=image_size[1],
+        min_height=image_size[0],
         border_mode=0,
         p=1.0,
         ),
@@ -178,9 +109,10 @@ load_pipeline = [
     dict(
         type='Resize',
         img_scale=image_size,
-        ratio_range=(0.8, 1.25),
-        multiscale_mode='range',
-        keep_ratio=True),
+        # ratio_range=(0.8, 1.25),
+        # multiscale_mode='range',
+        keep_ratio=False,
+        ),
     dict(
         type='RandomCrop',
         crop_type='absolute_range',
@@ -189,6 +121,7 @@ load_pipeline = [
         allow_negative_crop=True),
     dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
     dict(type='RandomFlip', flip_ratio=0.5),
+    # dict(type='Pad', size_divisor=32),
     dict(type='Pad', size=image_size),
 ]
 
@@ -228,34 +161,6 @@ train_pipeline = [
 
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    # dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    # dict(
-        # type='FineTuneCopyPaste', 
-        # max_num_pasted=100,
-        # supl_dataset_cfg=dict(
-            # ann_file='/home/nils/datasets/cars/coco/val.json',
-            # data_root='/home/nils/datasets/cars/',
-            # img_prefix='raw',
-            # pipeline=[
-                # dict(type='LoadImageFromFile', file_client_args=file_client_args),
-                # dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-                # dict(
-                    # type='Albu',
-                    # transforms=albu_val_transforms,
-                    # bbox_params=dict(
-                        # type='BboxParams',
-                        # format='pascal_voc',
-                        # label_fields=['gt_labels'],
-                        # min_visibility=0.0,
-                        # filter_lost_elements=True,
-                    # ),
-                    # update_pad_shape=False,
-                    # skip_img_without_anno=True,
-                # ),
-            # ],
-            # classes=CLASSES,
-        # ),
-    # ),
     dict(
         type='MultiScaleFlipAug',
         img_scale=(1333, 800),
@@ -269,12 +174,12 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
+
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type='MultiImageMixDataset',
-        # _delete_=True,
         dataset=dict(
             type=dataset_type,
             ann_file=data_root + 'annotations/instances_train2017.json',
@@ -286,12 +191,12 @@ data = dict(
     val=dict(
         type=dataset_type,
         classes=CLASSES,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file='/home/nils/datasets/cars/val_data/val.json',
+        img_prefix='/home/nils/datasets/cars/val_data/img/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         classes=CLASSES,
-        ann_file=data_root + 'annotations/instances_val2017.json',    # we don't have annotations for test2017
-        img_prefix=data_root + 'val2017/',
+        ann_file='/home/nils/datasets/cars/val_data/val.json',
+        img_prefix='/home/nils/datasets/cars/val_data/img/',
         pipeline=test_pipeline))
