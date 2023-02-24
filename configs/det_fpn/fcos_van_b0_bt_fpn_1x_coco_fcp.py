324b1f4bb2350b32641f9bbbafc0a345bf38322d
Cpt.Captain <nilskoch@posteo.de>
2023-02-24 10:56:03 +0100

    add configs for barlowtwins pretrained models

---
+++ configs/det_fpn/fcos_van_b0_bt_fpn_1x_coco_fcp.py
@@ -0,0 +1,110 @@
+_base_ = [
+    '../_base_/models/fcos_van_fpn.py',
+    # '../_base_/datasets/coco_detection.py',
+    '../_base_/datasets/finetune_coco_detection.py',
+    '../_base_/default_runtime.py',    
+    # '../_base_/schedules/schedule_4k.py',
+    '../_base_/schedules/schedule_1x_adam.py',
+]
+
+dims = [32, 64, 160, 256]
+# pretrained = dict(type='Pretrained', checkpoint='models/van_b0_bt_300.pth', prefix='backbone.')
+pretrained = dict(type='Pretrained', checkpoint='models/van_b0_bt_small_neck_1000.pth', prefix='backbone.')
+# pretrained = dict(type='Pretrained', checkpoint='models/van_b0_bt_thick_neck_1000.pth', prefix='backbone.')
+# pretrained = None
+norm_cfg = dict(type='SyncBN', requires_grad=not bool(pretrained))  # train BN only when not using pretrained model
+
+
+model = dict(
+    type='FCOS',
+    backbone=dict(
+        type='VAN',
+        _delete_=True,
+        arch='b0',
+        out_indices=[0,1,2,3],
+        # type='VAN_Official',
+        # embed_dims=dims,
+        # drop_rate=0.0,
+        # drop_path_rate=0.1,
+        # depths=[3, 3, 5, 2],
+        # norm_cfg=norm_cfg,
+        init_cfg=pretrained,
+      ),
+    neck=dict(
+        type='FPN',
+        in_channels=dims,
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_output',  # use P5
+        num_outs=5,
+        relu_before_extra_convs=True),
+    bbox_head=dict(
+        type='FCOSHead',
+        num_classes=80,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        strides=[8, 16, 32, 64, 128],
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='IoULoss', loss_weight=1.0),
+        loss_centerness=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.4,
+            min_pos_iou=0,
+            ignore_iof_thr=-1),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.5),
+        max_per_img=100)
+        )
+
+
+hooks=[
+    dict(type='TextLoggerHook', by_epoch=True),
+    dict(type='MMDetWandbHook', 
+      by_epoch=True, 
+      init_kwargs=dict(
+        entity="nkoch-aitastic",
+        project='van-detection', 
+        tags=[
+          'backbone:VAN-B0', 
+          'neck:FPN',
+          'head:FCOS', 
+          'pretrained',
+          'schedule:1x',
+          'finetune-copy-paste',
+          'barlow-twins:stanford-cars',
+          ]       
+      ),
+      interval=10,
+      log_checkpoint=True,
+      log_checkpoint_metadata=True,
+      num_eval_images=100,
+    ), # Check https://docs.wandb.ai/ref/python/init for more init arguments.
+    ]
+
+# hooks = [
+    # dict(type='TextLoggerHook', by_epoch=True),
+# ]
+log_config = dict(
+    hooks=hooks,
+    )
+
+workflow = [('train', 1)]
+data = dict(samples_per_gpu=4)
+
