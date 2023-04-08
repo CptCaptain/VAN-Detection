58daae6c7b37d2a5ffd9f5a309689141e7a807f7
Cpt.Captain <nilskoch@posteo.de>
2023-04-08 12:15:12 +0200

    remove unnecessary functions

--- load_and_save_dataset.py
+++ load_and_save_dataset.py
@@ -85,63 +85,6 @@ def get_coco_json_format():
     return coco_format
 
 
-def create_sub_masks(mask_image, width, height):
-    # Initialize a dictionary of sub-masks indexed by RGB colors
-    sub_masks = {}
-    for x in range(width):
-        for y in range(height):
-            # Get the BW values of the pixel
-            pixel = mask_image.getpixel((x, y))
-            if pixel == 0:
-                continue
-
-            # Check to see if we have created a sub-mask...
-            pixel_str = str(pixel)
-            sub_mask = sub_masks.get(pixel_str)
-            if sub_mask is None:
-                # Create a sub-mask (one bit per pixel) and add to the dictionary
-                # Note: we add 1 pixel of padding in each direction
-                # because the contours module doesn"t handle cases
-                # where pixels bleed to the edge of the image
-                sub_masks[pixel_str] = Image.new("1", (width + 2, height + 2))
-
-            # Set the pixel value to 1 (default is 0), accounting for padding
-            sub_masks[pixel_str].putpixel((x + 1, y + 1), 1)
-
-    return sub_masks
-
-
-def create_sub_mask_annotation(sub_mask):
-    # Find contours (boundary lines) around each sub-mask
-    # Note: there could be multiple contours if the object
-    # is partially occluded. (E.g. an elephant behind a tree)
-    contours = measure.find_contours(np.array(sub_mask), 0.5, positive_orientation="low")
-
-    polygons = []
-    segmentations = []
-    for contour in contours:
-        # Flip from (row, col) representation to (x, y)
-        # and subtract the padding pixel
-        for i in range(len(contour)):
-            row, col = contour[i]
-            contour[i] = (col - 1, row - 1)
-
-        # Make a polygon and simplify it
-        poly = Polygon(contour)
-        poly = poly.simplify(1.0, preserve_topology=False)
-
-        if poly.is_empty:
-            # Go to next iteration, dont save empty values in list
-            continue
-
-        polygons.append(poly)
-
-        segmentation = np.array(poly.exterior.coords).ravel().tolist()
-        segmentations.append(segmentation)
-
-    return polygons, segmentations
-
-
 def create_category_annotation(category_dict):
     category_list = []
 
@@ -239,6 +182,7 @@ def main():
 
         # Step three: generate annotation infos
         for i, (bbox, label, mask) in enumerate(zipper):
+            # bbox format: x, y, w, h
             bbox = [bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3] - bbox[1]]
             ann_info = {
                 'image_id': image_id,
