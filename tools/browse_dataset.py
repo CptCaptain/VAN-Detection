bbd76fadf647523baebea277c2cf99ba57b05936
Cpt.Captain <nilskoch@posteo.de>
2023-04-26 17:12:23 +0200

    update browse_dataset to be compatible with detection instead of segmentation

--- tools/browse_dataset.py
+++ tools/browse_dataset.py
@@ -1,48 +1,39 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 import os
-import warnings
+from collections import Sequence
 from pathlib import Path
 
 import mmcv
 import numpy as np
 from mmcv import Config, DictAction
 
-from mmseg.datasets.builder import build_dataset
+from mmdet.core.utils import mask2ndarray
+from mmdet.core.visualization import imshow_det_bboxes
+from mmdet.datasets.builder import build_dataset
+from mmdet.utils import replace_cfg_vals, update_data_root
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Browse a dataset')
     parser.add_argument('config', help='train config file path')
     parser.add_argument(
-        '--show-origin',
-        default=False,
-        action='store_true',
-        help='if True, omit all augmentation in pipeline,'
-        ' show origin image and seg map')
-    parser.add_argument(
         '--skip-type',
         type=str,
         nargs='+',
         default=['DefaultFormatBundle', 'Normalize', 'Collect'],
-        help='skip some useless pipeline，if `show-origin` is true, '
-        'all pipeline except `Load` will be skipped')
+        help='skip some useless pipeline')
     parser.add_argument(
         '--output-dir',
-        default='./output',
+        default=None,
         type=str,
         help='If there is no display interface, you can save it')
-    parser.add_argument('--show', default=False, action='store_true')
+    parser.add_argument('--not-show', default=False, action='store_true')
     parser.add_argument(
         '--show-interval',
-        type=int,
-        default=999,
-        help='the interval of show (ms)')
-    parser.add_argument(
-        '--opacity',
         type=float,
-        default=0.5,
-        help='the opacity of semantic map')
+        default=2,
+        help='the interval of show (s)')
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -57,124 +48,90 @@ def parse_args():
     return args
 
 
-def imshow_semantic(img,
-                    seg,
-                    class_names,
-                    palette=None,
-                    win_name='',
-                    show=False,
-                    wait_time=0,
-                    out_file=None,
-                    opacity=0.5):
-    """Draw `result` over `img`.
+def retrieve_data_cfg(config_path, skip_type, cfg_options):
 
-    Args:
-        img (str or Tensor): The image to be displayed.
-        seg (Tensor): The semantic segmentation results to draw over
-            `img`.
-        class_names (list[str]): Names of each classes.
-        palette (list[list[int]]] | np.ndarray | None): The palette of
-            segmentation map. If None is given, random palette will be
-            generated. Default: None
-        win_name (str): The window name.
-        wait_time (int): Value of waitKey param.
-            Default: 0.
-        show (bool): Whether to show the image.
-            Default: False.
-        out_file (str or None): The filename to write the image.
-            Default: None.
-        opacity(float): Opacity of painted segmentation map.
-            Default 0.5.
-            Must be in (0, 1] range.
-    Returns:
-        img (Tensor): Only if not `show` or `out_file`
-    """
-    img = mmcv.imread(img)
-    img = img.copy()
-    if palette is None:
-        palette = np.random.randint(0, 255, size=(len(class_names), 3))
-    palette = np.array(palette)
-    assert palette.shape[0] == len(class_names)
-    assert palette.shape[1] == 3
-    assert len(palette.shape) == 2
-    assert 0 < opacity <= 1.0
-    color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
-    for label, color in enumerate(palette):
-        color_seg[seg == label, :] = color
-    # convert to BGR
-    color_seg = color_seg[..., ::-1]
-
-    img = img * (1 - opacity) + color_seg * opacity
-    img = img.astype(np.uint8)
-    # if out_file specified, do not show image in window
-    if out_file is not None:
-        show = False
-
-    if show:
-        mmcv.imshow(img, win_name, wait_time)
-    if out_file is not None:
-        mmcv.imwrite(img, out_file)
-
-    if not (show or out_file):
-        warnings.warn('show==False and out_file is not specified, only '
-                      'result image will be returned')
-        return img
-
-
-def _retrieve_data_cfg(_data_cfg, skip_type, show_origin):
-    if show_origin is True:
-        # only keep pipeline of Loading data and ann
-        _data_cfg['pipeline'] = [
-            x for x in _data_cfg.pipeline if 'Load' in x['type']
-        ]
-    else:
-        _data_cfg['pipeline'] = [
-            x for x in _data_cfg.pipeline if x['type'] not in skip_type
+    def skip_pipeline_steps(config):
+        config['pipeline'] = [
+            x for x in config.pipeline if x['type'] not in skip_type
         ]
 
-
-def retrieve_data_cfg(config_path, skip_type, cfg_options, show_origin=False):
     cfg = Config.fromfile(config_path)
+
+    # replace the ${key} with the value of cfg.key
+    cfg = replace_cfg_vals(cfg)
+
+    # update data root according to MMDET_DATASETS
+    update_data_root(cfg)
+
     if cfg_options is not None:
         cfg.merge_from_dict(cfg_options)
     train_data_cfg = cfg.data.train
-    if isinstance(train_data_cfg, list):
-        for _data_cfg in train_data_cfg:
-            while 'dataset' in _data_cfg and _data_cfg[
-                    'type'] != 'MultiImageMixDataset':
-                _data_cfg = _data_cfg['dataset']
-            if 'pipeline' in _data_cfg:
-                _retrieve_data_cfg(_data_cfg, skip_type, show_origin)
-            else:
-                raise ValueError
+    while 'dataset' in train_data_cfg and train_data_cfg[
+            'type'] != 'MultiImageMixDataset':
+        train_data_cfg = train_data_cfg['dataset']
+
+    if isinstance(train_data_cfg, Sequence):
+        [skip_pipeline_steps(c) for c in train_data_cfg]
     else:
-        while 'dataset' in train_data_cfg and train_data_cfg[
-                'type'] != 'MultiImageMixDataset':
-            train_data_cfg = train_data_cfg['dataset']
-        _retrieve_data_cfg(train_data_cfg, skip_type, show_origin)
+        skip_pipeline_steps(train_data_cfg)
+
     return cfg
 
 
 def main():
     args = parse_args()
-    cfg = retrieve_data_cfg(args.config, args.skip_type, args.cfg_options,
-                            args.show_origin)
+    cfg = retrieve_data_cfg(args.config, args.skip_type, args.cfg_options)
+
+    if 'gt_semantic_seg' in cfg.train_pipeline[-1]['keys']:
+        cfg.data.train.pipeline = [
+            p for p in cfg.data.train.pipeline if p['type'] != 'SegRescale'
+        ]
     dataset = build_dataset(cfg.data.train)
+
     progress_bar = mmcv.ProgressBar(len(dataset))
+
     for item in dataset:
         filename = os.path.join(args.output_dir,
                                 Path(item['filename']).name
                                 ) if args.output_dir is not None else None
-        imshow_semantic(
+
+        gt_bboxes = item['gt_bboxes']
+        gt_labels = item['gt_labels']
+        gt_masks = item.get('gt_masks', None)
+        gt_masks = None
+        if gt_masks is not None:
+            gt_masks = mask2ndarray(gt_masks)
+
+        # gt_seg = item.get('gt_semantic_seg', None)
+        gt_seg = None
+        if gt_seg is not None:
+            pad_value = 255  # the padding value of gt_seg
+            sem_labels = np.unique(gt_seg)
+            all_labels = np.concatenate((gt_labels, sem_labels), axis=0)
+            all_labels, counts = np.unique(all_labels, return_counts=True)
+            stuff_labels = all_labels[np.logical_and(counts < 2,
+                                                     all_labels != pad_value)]
+            stuff_masks = gt_seg[None] == stuff_labels[:, None, None]
+            gt_labels = np.concatenate((gt_labels, stuff_labels), axis=0)
+            gt_masks = np.concatenate((gt_masks, stuff_masks.astype(np.uint8)),
+                                      axis=0)
+            # If you need to show the bounding boxes,
+            # please comment the following line
+            gt_bboxes = None
+
+        imshow_det_bboxes(
             item['img'],
-            item['gt_semantic_seg'],
-            dataset.CLASSES,
-            dataset.PALETTE,
-            show=args.show,
+            gt_bboxes,
+            gt_labels,
+            gt_masks,
+            class_names=dataset.CLASSES,
+            show=not args.not_show,
             wait_time=args.show_interval,
             out_file=filename,
-            opacity=args.opacity,
-        )
+            bbox_color=dataset.PALETTE,
+            text_color=(200, 200, 200),
+            mask_color=dataset.PALETTE)
+
         progress_bar.update()
 
 
